a5634fc15e0d2010d8bfa26239ea0ca96d1e1dac
Conrad Trost <ctrost@tamu.edu>
2020-07-07 17:39:49 -0500

    Frontend fix and added sass

--- public/javascript/add-mileage.js
+++ public/javascript/add-mileage.js
@@ -48,11 +48,10 @@ async function destroy(mileageData) {
 // Add in a way to pull user information to determine their type of oil and only display that type
 
 async function deleteRow(row_id) {
-  const date = document.querySelector("#mile_book").rows[row_id].cells[0].textContent;
+  // const date = document.querySelector("#mile_book").rows[row_id].cells[0].textContent;
   const mileage = document.querySelector("#mile_book").rows[row_id].cells[1].textContent;
-  console.log(date);
   console.log(mileage);
-  await destroy({ date, mileage })
+  await destroy({ mileage })
   // document.querySelector("#mile_book").deleteRow(row_id);
   location.reload();
 };
@@ -90,7 +89,7 @@ async function renderTable() {
       // Changed from if (i.mileage <= convOil)
       if ((i.mileage < 300) || (i.mileage % convOil) > 300) {
         answer1 = convOil - (i.mileage % convOil);
-        td3.innerText = answer1 + " more miles to go";
+        td3.innerText = answer1 + " miles to go";
 
       } else {
         // color code RED if over 3000
@@ -101,7 +100,7 @@ async function renderTable() {
       const td4 = document.createElement("td");
       if ((i.mileage < 300) || (i.mileage % synOil) > 300) {
         answer2 = synOil - (i.mileage % synOil);
-        td4.innerText = answer2 + " more miles to go";
+        td4.innerText = answer2 + " miles to go";
 
       } else {
         td4.innerText = "Change Oil!";
@@ -111,7 +110,7 @@ async function renderTable() {
       const td5 = document.createElement("td");
       if ((i.mileage < 300) || (i.mileage % brakes) > 300) {
         answer3 = brakes - (i.mileage % brakes);
-        td5.innerText = answer3 + " more miles to go";
+        td5.innerText = answer3 + " miles to go";
 
       } else {
         td5.innerText = "Change Brake Pads!";
@@ -122,7 +121,7 @@ async function renderTable() {
       const td6 = document.createElement("td");
       if ((i.mileage < 300) || (i.mileage % tires) > 300) {
         answer4 = tires - (i.mileage % tires);
-        td6.innerText = answer4 + " more miles to go";
+        td6.innerText = answer4 + " miles to go";
 
       } else {
         td6.innerText = "Change Tires!";
@@ -132,7 +131,7 @@ async function renderTable() {
       const td7 = document.createElement("td");
       if ((i.mileage < 300) || (i.mileage % filter) > 300) {
         answer5 = filter - (i.mileage % filter);
-        td7.innerText = answer5 + " more miles to go";
+        td7.innerText = answer5 + " miles to go";
         ;
       } else {
         td7.innerText = "Change Filter!";
@@ -152,12 +151,13 @@ async function renderTable() {
 document.getElementById("add-mileage")
   .addEventListener('click', async () => {
     // get form data
-    const date = document.getElementById("date_miles").value;
+    // Date comes from model now
     const mileage = parseInt(document.getElementById("miles").value);
     const user_id = JSON.parse(localStorage.getItem('user')).id;
     console.log(user_id)
     // post to api with the form data
-    await post({ date, mileage, user_id });
+    await post({ mileage, user_id })
+    location.reload();
   });
 
 document.addEventListener("DOMContentLoaded", renderTable);
